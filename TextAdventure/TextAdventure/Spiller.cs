a54ec8f80fab0367bffb41a3b4ecc9f8e5abee2b
CakeMonster987 <mikkelboegh15@gmail.com>
2023-11-02 11:22:57 +0100

    Angreb update + evade

--- TextAdventure/TextAdventure/Spiller.cs
+++ TextAdventure/TextAdventure/Spiller.cs
@@ -10,19 +10,26 @@ namespace TextAdventure
     {
         public string Name { get; }
         public int HP = 10;
+        public Items Våben {  get; }
+        public bool evade = false;
 
         public Spiller(string name)
         {
             Name = name;
         }
-        public void Angreb (Fjender enemy, Items våben)
+        public void Angreb (Fjender enemy)
         {
             Console.WriteLine("Du bruger dit våben til at slå ud efter " + enemy.Name);
-            Console.WriteLine("Du rammer og gør " + våben.Damage +" skade");
-            enemy.HP -= våben.Damage;
+            Console.WriteLine("Du rammer og gør "+ Våben.Damage +" skade");
+            enemy.HP -= Våben.Damage;
 
         }
 
+        public void Evade()
+        {
+            Console.WriteLine("Du undviger");
+            evade = true;
+        }
 
     }
 }
