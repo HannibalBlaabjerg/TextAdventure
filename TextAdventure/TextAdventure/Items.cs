45ac5075dde1963b09e783e4364d5a6f16288ab7
HannibalBlaabjerg <hannibal@blab.dk>
2023-12-06 12:34:34 +0100

    ghjk

--- TextAdventure/TextAdventure/Items.cs
+++ TextAdventure/TextAdventure/Items.cs
@@ -12,10 +12,10 @@ namespace TextAdventure
         //Jeg laver derefter en liste til mine Items der hedder items
         public static List<Items> items = new List<Items>
         {
-            new Items("Sværd", "Et skarpt sværd du kan bruge til at beskytte dig selv.", 1),
-            new Items("Sten", "En sten du kan bruge til BONKE folk.", 4),
-            new Items("Sej Pind", "En pind der gør dig til den sejeste på lejepladsen!!!", 8),
-            new Items("Drage Klinge", "Det er egentlig meget simpelt, det er det bedste våben", 1000)
+            new Items("Sværd", " du kan bruge til at beskytte dig selv.", 1),
+            new Items("Sten", " du kan bruge til BONKE folk.", 4),
+            new Items("Sej Pind", " der gør dig til den sejeste på lejepladsen!!!", 8),
+            new Items("Drage Klinge", " Det er egentlig meget simpelt, det er det bedste våben", 1000)
         };
         //Jeg laver en string variabel der hedder name
         public string Name;
